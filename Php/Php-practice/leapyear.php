411f893424925cc3e93199b4823faf73366fb32b
Farjanaepa <farjanaepa@gmail.com>
2024-08-05 00:58:49 +0600

    php practice

---
+++ Php/Php-practice/leapyear.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$year = 2016 ;
+
+if ($year % 4 == 0 && $year % 100 == 0 && $year % 400 == 0) {
+
+    echo"{$year} is a leap year";
+}
+elseif ($year %4 == 0 && $year % 100 == 0) {
+    echo"{$year} is not a leap year";
+}elseif ($year %4 == 0) {
+    echo"{$year} is a leap year";
+}
+else {
+    echo"{$year} is not a leap year";
+}
+
+
+?>
+
