678e708a115d56afdeac225c72a439325888b174
Farjanaepa <farjanaepa@gmail.com>
2024-08-07 01:57:26 +0600

    oop practice

---
+++ Php/OOP/Class-properties-methods/class-properties-methods-use.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class calculation{
+
+    // public -> class variable can access outside the class with object
+    public $a, $b, $c;   //----> Properties
+
+    function sum(){    //-----> Methods
+        $this->c = $this->a + $this->b;
+        return $this->c;
+    }
+
+    function sub(){    //------> Methods
+        $this->c = $this->a - $this->b;
+        return $this->c;
+    }
+}
+//$c1 is called object
+
+$c1 = new calculation();
+$c1->a = 20;
+$c1->a = 10;
+
+
+
+$c2 = new calculation();
+$c2->a = 23;
+$c2->a = 35;
+
+echo "Sum value of c1 :" . $c1->sum() . "\n";
+
+echo "Substraction value of c2 :" . $c1->sub();
+?>
