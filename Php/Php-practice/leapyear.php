40cf120cf121eba25b42f4b0a1081097c8993486
Farjanaepa <152874011+Farjanaepa@users.noreply.github.com>
2024-10-20 23:42:41 +0600

    Update leapyear.php

--- Php/Php-practice/leapyear.php
+++ Php/Php-practice/leapyear.php
@@ -10,7 +10,7 @@ if ($year % 4 == 0 && $year % 100 == 0 && $year % 400 == 0) {
 elseif ($year %4 == 0 && $year % 100 == 0) {
     echo"{$year} is not a leap year";
 }elseif ($year %4 == 0) {
-    echo"{$year} is a leap year";
+    echo"{$year} is a leap year ";
 }
 else {
     echo"{$year} is not a leap year";
